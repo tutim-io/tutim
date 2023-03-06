80c5f2fc4ce5b9710a64fc3ca7eddd5d5aa05eb2
eylonmiz <eylonmizrahi97@gmail.com>
2023-03-06 20:43:48 +0200

    fix(): wizard layout fix

--- packages/fields/src/Forms/FormElement.tsx
+++ packages/fields/src/Forms/FormElement.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { FieldsByKey, FormLayout, FormMeta } from '@tutim/types';
 import { FormGrid } from './FormGrid';
 import { SubmitButton } from '../Buttons';
-import { FormTabs } from '../Tabs';
 import { getFieldsFromMap } from '@tutim/headless';
 import { getGroupFields } from './getGroupFields';
 import { Typography } from '@mui/material';
@@ -28,30 +27,10 @@ export const getFieldsLayout = ({
   return getGroupFields(layout, fieldsByKey);
 };
 
-const WizardLayout = ({ layout = {}, fieldsByKey }: Pick<FormProps, 'layout' | 'fieldsByKey'>) => {
-  const { groups = [] } = layout.groupConfigs || {};
-  const labels = groups.map((group: any) => group.title ?? group.key);
-  const groupFields = getGroupFields(layout, fieldsByKey);
-
-  return (
-    <FormTabs labels={labels}>
-      {groups.map((group: any, ix: number) => {
-        return (
-          <FormGrid key={group.key}>{[groupFields[ix], <SubmitButton key="submit" {...layout.submit} />]}</FormGrid>
-        );
-      })}
-    </FormTabs>
-  );
-};
-
 export const FormElement = ({ onSubmit, layout = {}, meta = {}, fieldsByKey, formId }: FormProps) => {
   const title = meta.title && <Typography variant="h5">{meta.title}</Typography>;
 
-  const fields = layout.wizard ? (
-    <WizardLayout layout={layout} fieldsByKey={fieldsByKey} />
-  ) : (
-    <FormGrid>{fieldsLayout({ layout, fieldsByKey })}</FormGrid>
-  );
+  const fields = <FormGrid>{fieldsLayout({ layout, fieldsByKey })}</FormGrid>;
 
   return (
     <form onSubmit={onSubmit} style={layout.style} noValidate id={formId}>
