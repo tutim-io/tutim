e6b6f506669aac8cef3592a9b275925599c8b449
eylonmiz <eylonmizrahi97@gmail.com>
2023-04-11 12:05:44 +0300

    fix(header-overflow): fix

--- packages/fields/src/Forms/MultiStepWizard/Header.tsx
+++ packages/fields/src/Forms/MultiStepWizard/Header.tsx
@@ -7,18 +7,20 @@ import { PartialFormConfig } from '@tutim/types';
 export const Header = ({ config }: { config: PartialFormConfig }) => {
   const { activeStep, goToStep } = useWizard();
   if (!config.wizard) return null;
+  const isVertical = config.wizard.orientation === 'vertical';
+  const styling = isVertical
+    ? { overflow: 'auto', height: '100%', minWidth: '100px', maxWidth: '250px', padding: '10px' }
+    : { overflow: 'auto', width: '100%', height: '80px', padding: '10px' };
 
   return (
-    <Stepper
-      nonLinear
-      activeStep={activeStep}
-      orientation={config.wizard.orientation}
-      id="wizard-header"
-      sx={{ height: '60px' }}
-    >
+    <Stepper nonLinear activeStep={activeStep} orientation={config.wizard.orientation} id="wizard-header" sx={styling}>
       {Object.values(config.wizard.steps).map(({ label }, index) => (
         <Step key={label}>
-          <StepButton color="inherit" onClick={() => goToStep(index)}>
+          <StepButton
+            color="inherit"
+            style={{ maxWidth: '300px', textOverflow: 'clip' }}
+            onClick={() => goToStep(index)}
+          >
             {label}
           </StepButton>
         </Step>
