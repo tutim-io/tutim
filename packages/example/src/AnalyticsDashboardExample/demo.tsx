b30d89e0fab0a0fa6d16d232562dd90207ef5a8c
eylonmiz <eylonmizrahi97@gmail.com>
2023-06-20 20:16:52 +0300

    feat(shadcn-ui): app example

--- packages/example/src/AnalyticsDashboardExample/demo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AnalyticsDashboardPage from './index';
-
-const AnalyticsDashboardPageDemo: React.FC = () => {
-  return <AnalyticsDashboardPage />;
-};
-
-export default AnalyticsDashboardPageDemo;
