0e08d72adada8fbe776e7786840a55f1eea98a99
eylonmiz <eylonmizrahi97@gmail.com>
2023-06-20 19:28:07 +0300

    chore(renaming-form-provider): cleanup

--- packages/fields/src/Forms/MultiStepWizard/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './MultiStepWizard';
-export * from './use-wizard';
--- packages/fields/src/Forms/index.ts
+++ packages/fields/src/Forms/index.ts
@@ -2,4 +2,3 @@ export * from './FormElement';
 export * from './FormGrid';
 export * from './FieldGroup';
 export * from './Form';
-export * from './MultiStepWizard';
