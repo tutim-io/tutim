13807def62c70c75469bdde94db9cfee416ab0ce
Lior <lior4007@gmail.com>
2023-06-25 14:08:46 +0300

    fix(components): remove tests

--- packages/shadcn-ui/src/exports/Fields/TextField.tsx
+++ packages/shadcn-ui/src/exports/Fields/TextField.tsx
@@ -3,8 +3,6 @@ import { Field } from '@tutim/types';
 import { FieldWrapper } from './FieldWrapper';
 import { Input } from "../../components/ui/input"
 
-import { FieldCollapse } from '../Forms/Collapse';
-
 export const TextField: Field = ({ fieldConfig, inputProps: { value, onChange }, fieldState }) => {
   const { key, isRequired, isDisabled, placeholder } = fieldConfig;
 
@@ -24,16 +22,6 @@ export const TextField: Field = ({ fieldConfig, inputProps: { value, onChange },
         className='mt-2 mb-1'
       />
 
-      <FieldCollapse
-        key={1}
-        error={'error'}
-        title={
-          <>
-            <span style={{ fontSize: '20px' }}>testttt</span>
-            
-          </>
-        }
-      />
     </FieldWrapper>
   );
 };
