125308625354bf027ad46defc7c3408c622c0eee
Tamara Seban <tseban@g.jct.ac.il>
2023-07-12 12:07:28 +0300

    Updating SelectField to be using shadcn-ui instead of materialUi

--- packages/shadcn-ui/src/exports/Fields/SelectField.tsx
+++ packages/shadcn-ui/src/exports/Fields/SelectField.tsx
@@ -1,4 +1,4 @@
-import { Field } from '@tutim/types';
+import { Option, Field } from '@tutim/types';
 import { FieldWrapper } from './FieldWrapper';
 import {
   Select,
@@ -6,23 +6,32 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
+  SelectGroup
 } from '../../components/ui/select';
 
+export const SelectField: Field = ({ fieldConfig, inputProps: { value, onChange }, fieldState }) => {
+  
+  const { key, isRequired, isDisabled, options = [] } = fieldConfig;
 
-export const SelectField: Field = ({ fieldConfig, inputProps: fieldState }) => { 
-  fieldConfig;
+
+  const childOptions = options.map((option: Option) => (
+     <SelectItem value={ String(option.value)} key={option.value} disabled={option.disabled}>{option.label}</SelectItem>
+  ));
+
+
   return (
     <FieldWrapper fieldConfig={fieldConfig} fieldState={fieldState}>
-      <Select>
-  <SelectTrigger className="w-[180px]">
-    <SelectValue placeholder="Editor" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="Administrator">Administrator</SelectItem>
-    <SelectItem value="Viewer" disabled>Viewer</SelectItem>
-    <SelectItem value="Editor">Editor</SelectItem>
-  </SelectContent>
-</Select>
+      <div  className="mt-2" > 
+        <Select key={key} required ={isRequired} disabled={isDisabled} onValueChange ={onChange}>
+      <SelectTrigger className="w-full">
+        <SelectValue placeholder={value || ''} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {childOptions}
+        </SelectGroup>
+      </SelectContent>
+    </Select></div>  
     </FieldWrapper>
   );
 };
